7c4b1cd61bd20ad31aa01fe24c2bb6241ebe60ea
uloydev <uloydev@gmail.com>
2024-07-31 01:08:20 +0700

    make personel gear collection page responsive

--- src/routes/about/[personel]/gears/+page.svelte
+++ src/routes/about/[personel]/gears/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="pt-20">
-	<div class="text-madblue font-anton underline text-3xl flex gap-x-4 capitalize mb-10">
+	<div class="text-madblue font-anton underline flex capitalize mb-2 text-sm gap-x-1 sm:mb-10 sm:text-3xl sm:gap-x-4">
 		<a href="/about">About</a>
 		<span>&gt;</span>
 		<a href="/about/{data.name}">{data.name}</a>
@@ -12,21 +12,21 @@
 		<a href="/about/{data.name}/gears">Gear Collection</a>
 	</div>
 	<div class="mb-8">
-		<div class="flex justify-start gap-x-4 bg-madred items-center mb-6">
-			<div class="relative w-full max-w-[10%] h-[130px]">
-				<img src={data.toolsIcon} alt="tools icon" class="h-[150px] absolute bottom-0" />
+		<div class="flex justify-start gap-x-4 bg-madred items-center mb-2 sm:mb-6">
+			<div class="relative w-1/4 sm:w-full sm:max-w-[10%] sm:h-[130px]">
+				<img src={data.toolsIcon} alt="tools icon" class="w-full sm:h-[150px] sm:absolute sm:bottom-0" />
 			</div>
 			<div class="font-anton uppercase flex-grow">
-				<p class="text-6xl italic text-white mb-2">gear collections</p>
-				<p class="text-2xl underline text-madyellow">by {data.name}</p>
+				<p class="italic text-white mb-2 text-3xl sm:text-6xl">gear collections</p>
+				<p class="underline text-madyellow text-sm sm:text-2xl">by {data.name}</p>
 			</div>
 		</div>
-		<div class="grid grid-cols-3 gap-6">
+		<div class="grid grid-cols-2 gap-2 sm:grid-cols-3 sm:gap-6">
 			{#each data.tools as tool}
-				<div class="col-span-1 p-4 bg-madsecondary h-full">
-					<img src={tool.image} alt={tool.title} class="w-full aspect-square mb-4">
-					<p class="font-anton text-madyellow text-3xl mb-4">{tool.title}</p>
-					<p class="font-roboto text-white text-lg font-extrabold mb-4">{tool.title}</p>
+				<div class="col-span-1 bg-madsecondary h-full p-2 sm:p-4">
+					<img src={tool.image} alt={tool.title} class="w-full aspect-square mb-2 sm:mb-4">
+					<p class="font-anton text-madyellow text-sm sm:mb-4 sm:text-3xl">{tool.title}</p>
+					<p class="font-roboto text-white font-extrabold text-[10px] mb-4 sm:text-lg ">{tool.title}</p>
 				</div>
 			{/each}
 		</div>
